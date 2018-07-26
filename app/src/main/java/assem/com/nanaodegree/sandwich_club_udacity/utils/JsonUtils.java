6a1df9d44dbce125208b38dae0c3c0ff00851f49
Mohamed Assem <mo7mad.assim@gmail.com>
2018-07-26 22:42:49 +0200

    Initial commit

---
+++ app/src/main/java/assem/com/nanaodegree/sandwich_club_udacity/utils/JsonUtils.java
@@ -0,0 +1,15 @@
+package assem.com.nanaodegree.sandwich_club_udacity.utils;
+
+import org.json.JSONObject;
+
+import assem.com.nanaodegree.sandwich_club_udacity.model.Sandwich;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        JSONObject jsonObject = new JSONObject();
+//        JSONObject nameObject = jsonObject.getString("")
+        Sandwich sandwich = new Sandwich();
+        return null;
+    }
+}
