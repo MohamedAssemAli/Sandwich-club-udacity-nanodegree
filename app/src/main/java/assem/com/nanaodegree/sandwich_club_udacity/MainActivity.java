8d817dd8a0fce89439202afda3e8d6f17bfd6e49
Mohamed Assem <mo7mad.assim@gmail.com>
2018-07-26 22:02:20 +0200

    Initial commit

---
+++ app/src/main/java/assem/com/nanaodegree/sandwich_club_udacity/MainActivity.java
@@ -0,0 +1,13 @@
+    package assem.com.nanaodegree.sandwich_club_udacity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
